e2979d856a5bb14dcf2144c1e4026814ca7e99e0
github username <github email address>
2020-09-10 11:55:09 -0400

    categories

--- back-end/app/controllers/categories_controller.rb
+++ back-end/app/controllers/categories_controller.rb
@@ -1,2 +1,22 @@
 class CategoriesController < ApplicationController
+
+    def index
+        categories = Category.all
+        render json: categories, include: :jokes
+    
+    end
+
+    def create
+        category = Category.create(category_params)
+        render json: category
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:title)
+    end
+
+
+
 end
