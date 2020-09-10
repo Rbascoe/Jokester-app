e8b198176695fdc3d86d7291ac4508a4ad6f3b16
github username <github email address>
2020-09-10 10:42:40 -0400

    joke form with user

--- front-end/index.js
+++ front-end/index.js
@@ -2,56 +2,47 @@ const usersUrl = "http://localhost:3000/users"
 const jokesUrl = "http://localhost:3000/jokes"
 
 const ul = document.querySelector('ul#joke-list')
+const selector = document.querySelector('select#user-selector')
 const jokeForm = document.querySelector('form.create-joke')
 
 function getUsers(){
     fetch(usersUrl)
     .then(res => res.json())
-    .then(users => users.forEach(users => showJokes(users)))
+    .then(users => showJokes(users))
 }
 
 getUsers()
 
 function showJokes(users){
+    users.forEach(user => 
+        user.jokes.forEach(jokes => {const jokeLi = document.createElement('li') 
+        const footer = document.createElement('footer')
+        footer.innerText = user.username
+        jokeLi.innerText = jokes.description
+        jokeLi.append(footer)
+        ul.append(jokeLi) })
+    )
+    users.forEach(user => {
+        const userSelector = document.createElement('option')
+        userSelector.innerText = user.username
+        userSelector.value = user.id
+        selector.append(userSelector)
+    })
 
-    users.jokes.forEach(jokes => {const jokeLi = document.createElement('li') 
-    const footer = document.createElement('footer')
-    footer.innerText = users.username
-    jokeLi.innerText = jokes.description
-    jokeLi.append(footer)
-    ul.append(jokeLi) })
-
-    
-
-}
 
 jokeForm.addEventListener('submit', () => {
     event.preventDefault()
     //console.log(event.target)
     let descriptionInput = event.target[0].value
-    // let descript = users.jokes.forEach(description => {
-    //     description.description
-    // })
-
-    //Trying to see if we can make an if/switch statement to check 
-    // username against values in dropdown and assign the 
-    // appropriate user_id
-
-    let user_id = {
-    
+    let authorInput = event.target[1].value
+    let authorName = ""
+    for (let i in users){
+        if (users[i].id == authorInput){
+            authorName = users[i].username
+            break
+        }
     }
 
-    // let configObj = {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'Accept': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //         description: descriptionInput,
-    //         user: authorInput
-    //     })
-    // }
     fetch(jokesUrl, {
         method: 'POST',
         headers: {
@@ -62,11 +53,23 @@ jokeForm.addEventListener('submit', () => {
             description: descriptionInput,
             laughs: 0,
             frowns: 0, 
-            user_id: users.username
+            user_id: authorInput
+            //category: categoryInput
         })
     })
     .then(res => res.json())
-    .then(console.log)
+    .then(
+        newJoke => {
+        newJokeLi = document.createElement('li')
+        footer = document.createElement('footer')
+        newJokeLi.innerText = newJoke.description
+        footer.innerText = authorName
+        newJokeLi.append(footer)
+        ul.append(newJokeLi)
+        jokeForm.reset()
+    }
+    )
     
 })
 
+}
